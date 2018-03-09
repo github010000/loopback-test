4f3a019a9e6e7bbb38a92848d56024672e514fd6
newea <newea@DESKTOP-SOD5SIK>
2018-03-09 20:08:59 +0900

    efs

--- src/test/java/com/skb/xpg/nxpg/svc/NXPGSvcApplicationTests.java
+++ src/test/java/com/skb/xpg/nxpg/svc/NXPGSvcApplicationTests.java
@@ -1,18 +1,18 @@
-package com.skb.xpg.nxpg.svc;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles( profiles={"local"} )
-public class NXPGSvcApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.skb.xpg.nxpg.svc;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@ActiveProfiles( profiles={"local"} )
+//public class NXPGSvcApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
