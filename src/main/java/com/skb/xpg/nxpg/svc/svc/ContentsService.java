dfe7effce32f37cd0ce36c8858e62022e3275e61
newea <newea@DESKTOP-SOD5SIK>
2018-03-30 10:46:31 +0900

    efsefs

--- src/main/java/com/skb/xpg/nxpg/svc/svc/ContentsService.java
+++ src/main/java/com/skb/xpg/nxpg/svc/svc/ContentsService.java
@@ -1,7 +1,5 @@
 package com.skb.xpg.nxpg.svc.svc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
