02d5696d211cb7101911a03e0e1fe7e89e8ea9ac
dmshin <dmshin@zinnaworks.com>
2018-05-03 21:43:57 +0900

    asdfasdfas

--- src/main/java/com/skb/xpg/nxpg/svc/controller/ContentsController.java
+++ src/main/java/com/skb/xpg/nxpg/svc/controller/ContentsController.java
@@ -122,7 +122,7 @@ public class ContentsController {
 		rtn.put("IF", IF);
 		rtn.put("request_time", DateUtil.getYYYYMMDDhhmmss());
 
-		if (StrUtil.isEmpty(param.get("menu_id"))) {
+		if (StrUtil.isEmpty(param.get("sris_id"))) {
 			rtn.put("result", "9999");
 			rtn.put("reason", ResultCommon.reason.get(rtn.get("result")));
 			return rtn;
