fdabb22232574aea87f3fdb2f4219e3c10340768
[지나웍스] 신동민 <dmshin@zinnaworks.com>
2019-03-06 15:25:47 +0900

    [BTVD-4436] 소스 수정

--- src/main/java/com/skb/xpg/nxpg/svc/config/WebConfiguration.java
+++ src/main/java/com/skb/xpg/nxpg/svc/config/WebConfiguration.java
@@ -1,10 +1,8 @@
 package com.skb.xpg.nxpg.svc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,15 +18,4 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         registry.addInterceptor(certificationInterceptor)
                 .addPathPatterns("/**");
     }
-    
-//    @Override
-//    public void configureAsyncSupport(final AsyncSupportConfigurer configurer) {
-//        configurer.setDefaultTimeout(2000);
-//        configurer.registerCallableInterceptors(timeoutInterceptor());
-//    }
-//    
-//    @Bean
-//    public TimeoutCallableProcessingInterceptor timeoutInterceptor() {
-//        return new TimeoutCallableProcessingInterceptor();
-//    }
 }
