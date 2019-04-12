d67fa5177ac0f7e16f78cb5a16b2cc05fb0c3a12
unknown <Administrator@SKCC14D00789.SKCC.NET>
2019-04-12 14:36:26 +0900

    Second Init

--- src/main/java/com/jung/sys/controller/ErrorController.java
+++ src/main/java/com/jung/sys/controller/ErrorController.java
@@ -1,32 +1,40 @@
 package com.jung.sys.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jung.sys.common.ResultCommon;
-import com.jung.sys.config.Properties;
-import com.jung.sys.util.DateUtil;
+
 
 @RestController
 @RequestMapping(value = "/", produces = "application/json; charset=utf-8")
 public class ErrorController {
-	
-	@Autowired
-	private Properties properties;
+
 	
 	@RequestMapping(value = "/sleep")
 	public Map<String, Object> error(@RequestParam Map<String, String> param) {
-		Map<String, Object> result = properties.getResults();
+		
+        String millis = param.get("millis");
+        System.out.println("##### sleep entered...millis=" + millis);
 
-		result.put("IF", param.get("IF"));
-		result.put("result", "9998");
-		result.put("reason", ResultCommon.reason.get(result.get("result")));
-		result.put("response_time", DateUtil.getYYYYMMDDhhmmss());
-//		LogUtil.tlog(param.get("IF"), "SEND.RES", param.get("UUID"), param.get("stb_id"), "STB", result, param);
-		return result;
+        Map<String, Object> myResult = new HashMap<String, Object>();
+        myResult.put("svc_name", "SLEEP");
+        myResult.put("result", "0000");
+        myResult.put("millis", millis);
+
+        try {
+            Thread.sleep(Long.parseLong(millis));
+        }
+        catch(Exception e) {
+        	System.err.println(e.toString());
+            myResult.put("result", "-1");
+            myResult.put("reason", "Exception");
+        }
+
+        System.out.println("##### the end..." + myResult.toString());
+        return myResult;
 	}
 }
